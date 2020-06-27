29412754c622a85274213f831dae4e43c69450e9
Ryotaro <k.ryoutaro.g@gmail.com>
2020-06-27 14:42:53 +0900

    カンマが入ったタスク名の登録を防ぐバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
+  validate  :name_not_include_comma
+
+  private
+  def name_not_include_comma
+    errors.add(:name, :name_not_include_comma) if name&.include?(',')
+  end
 end
