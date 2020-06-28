68d904c38dee84a92bf63393e1b99c62ca3060c9
Ryotaro <k.ryoutaro.g@gmail.com>
2020-06-28 21:55:56 +0900

    ユーザーの新規作成機能の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,15 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_url(@user), notice: "ユーザーが「#{@user.name}」を登録しました"
+    else
+      render "new"
+    end
   end
 
   def edit
@@ -9,5 +19,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
   end
 end
