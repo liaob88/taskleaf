40f3a06679dc14d76ff6a133084ed547b561ed8b
Ryotaro <k.ryoutaro.g@gmail.com>
2020-06-29 10:24:12 +0900

    ユーザーの更新機能の作成と共通化した form の適用

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,13 +8,21 @@ class Admin::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to admin_user_url(@user), notice: "ユーザーが「#{@user.name}」を登録しました"
+      redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました"
     else
       render "new"
     end
   end
 
   def edit
+  end
+
+  def update
+    if @user.save?
+      redirect_to admin_user_path(@user), notice: "ユーザー「#{@user.name}」を更新しました"
+    else
+      render "edit"
+    end
   end
 
   def show
