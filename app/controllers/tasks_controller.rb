77ae1bb8d8f467144f9711250dbcc10df33e568a
Ryotaro <k.ryoutaro.g@gmail.com>
2020-07-01 07:27:15 +0900

    recent scope を task model に追加して task を昇順で返すメソッドを共通化した

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = current_user.tasks.order(created_at: :desc)
+    @tasks = current_user.tasks.recent
   end
 
   def new
