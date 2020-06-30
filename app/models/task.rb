f3465cc820641c92d8ff55d84efdbb544d6a1b22
Ryotaro <k.ryoutaro.g@gmail.com>
2020-06-30 22:01:05 +0900

    task model と user model の関連付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, length: { maximum: 30 }
   validate  :name_not_include_comma
 
+  belongs_to :user
+
   private
   def name_not_include_comma
     errors.add(:name, :name_not_include_comma) if name&.include?(',')
