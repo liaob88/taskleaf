36ad39e54e97571a4c5347d1d4e2e260758cb13d
Ryotaro <k.ryoutaro.g@gmail.com>
2020-06-30 13:07:09 +0900

    session 作成機能とログイン画面実装

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,24 @@
 class SessionController < ApplicationController
-  def new
+  def new    
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'ログインしました'
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
