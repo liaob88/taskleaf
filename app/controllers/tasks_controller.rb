76d1a498792e93306893eeab7f7af27bd371414b
Ryotaro <k.ryoutaro.g@gmail.com>
2020-07-07 22:24:13 +0900

    確認画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,11 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.new(task_params)
 
+    if params[:back].present?
+      render :new
+      return
+    end
+
     if @task.save
       redirect_to tasks_url(@task), notice: "タスク「#{@task.name}」を登録しました"
     else
@@ -33,6 +38,11 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy!
     redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました"
+  end
+
+  def confirm_new
+    @task = current_user.tasks.new(task_params)
+    render :new if !@task.valid?
   end
 
   private
