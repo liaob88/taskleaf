17c40aa8f86e8d15c12cecdbb97081c597b14e6b
Ryotaro <k.ryoutaro.g@gmail.com>
2020-07-07 22:31:26 +0900

    task 作成成功時の redirect 先の URL を間違えていたので修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,7 +21,7 @@ class TasksController < ApplicationController
     end
 
     if @task.save
-      redirect_to tasks_url(@task), notice: "タスク「#{@task.name}」を登録しました"
+      redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました"
     else
       render :new
     end
